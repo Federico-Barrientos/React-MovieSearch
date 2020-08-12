436083d039812f79c31546531e323be442c15f53
federico-barrientos <fgbarrientos93@gmail.com>
2020-08-12 18:51:26 -0300

    Updates

---
+++ precache-manifest.d3e4105791a370a0f59813258acb2288.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c945c022032d9119400c27d0b091300",
+    "url": "/React-MovieSearch/index.html"
+  },
+  {
+    "revision": "d56f58f6309cac3943b2",
+    "url": "/React-MovieSearch/static/css/main.ac389218.chunk.css"
+  },
+  {
+    "revision": "99d0ffb60db12e2458df",
+    "url": "/React-MovieSearch/static/js/2.9deade40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-MovieSearch/static/js/2.9deade40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d56f58f6309cac3943b2",
+    "url": "/React-MovieSearch/static/js/main.1d656557.chunk.js"
+  },
+  {
+    "revision": "bc7d8609801c560c197f",
+    "url": "/React-MovieSearch/static/js/runtime-main.186f497a.js"
+  }
+]);
